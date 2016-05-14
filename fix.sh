22fdcd230bab34980cfed1b97d78737a96f6f9d1
JakimLi <ljking7002@gmail.com>
2016-05-14 21:27:40 +0800

    put yaml conf file for alertmanager

--- fix.sh
+++ fix.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Use yaml config file for alertmanager..."
-mv ~/workspace/monitoring_config/alertmanager.conf ~/workspace/monitoring_config/alertmanager.conf.backup
 
+monitor_config=~/workspace/monitoring_config
+
+mv $monitor_config/alertmanager.conf $monitor_config/alertmanager.conf.backup
+curl -o $monitor_config/alertmanager.conf https://raw.githubusercontent.com/JakimLi/docker-workshop-m5-pathc/master/alertmanager.conf.yml
 
