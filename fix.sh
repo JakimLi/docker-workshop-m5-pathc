c47bff0a4f8027636434ece15899750363d57bac
JakimLi <ljking7002@gmail.com>
2016-05-14 21:37:04 +0800

    correct filename

--- fix.sh
+++ fix.sh
@@ -6,5 +6,6 @@ monitor_config=~/workspace/monitoring_config
 mv $monitor_config/alertmanager.conf $monitor_config/alertmanager.conf.backup
 curl -o $monitor_config/alertmanager.conf https://raw.githubusercontent.com/JakimLi/docker-workshop-m5-pathc/master/alertmanager.conf.yml
 
+echo "User new version alert rules file for server..."
 mv $monitor_config/workshop_alerting.rules $monitor_config/workshop_alerting.rules.backup
-curl -o $monitor_config/workshop_alerting.rules https://raw.githubusercontent.com/JakimLi/docker-workshop-m5-pathc/master/workshop_alerting.rules
+curl -o $monitor_config/workshop_alerting.rules https://raw.githubusercontent.com/JakimLi/docker-workshop-m5-pathc/master/workshop-alerting.rules
