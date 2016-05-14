d3bd7d28c5d2d501365d2290079079de0c1b7a15
JakimLi <ljking7002@gmail.com>
2016-05-14 21:23:06 +0800

    add alertmanager.conf.yml

--- fix.sh
+++ fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "Hello world"
+echo "Use yaml config file for alertmanager..."
+mv ~/workspace/monitoring_config/alertmanager.conf ~/workspace/monitoring_config/alertmanager.conf.backup
+
+
