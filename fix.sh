edc9718b1b6af2e2e99e23f5bcd5f2b38b93fac9
JakimLi <ljking7002@gmail.com>
2016-05-14 21:33:52 +0800

    add new verion alert rules file for promeheus server

--- fix.sh
+++ fix.sh
@@ -6,3 +6,5 @@ monitor_config=~/workspace/monitoring_config
 mv $monitor_config/alertmanager.conf $monitor_config/alertmanager.conf.backup
 curl -o $monitor_config/alertmanager.conf https://raw.githubusercontent.com/JakimLi/docker-workshop-m5-pathc/master/alertmanager.conf.yml
 
+mv $monitor_config/workshop_alerting.rules $monitor_config/workshop_alerting.rules.backup
+curl -o $monitor_config/workshop_alerting.rules https://raw.githubusercontent.com/JakimLi/docker-workshop-m5-pathc/master/workshop_alerting.rules
